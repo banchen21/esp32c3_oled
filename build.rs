83b38b29f941e3c72c4049f59bcbd155292ee03b
banchen21 <banchen19@outlook.com>
2024-10-16 05:46:45 +0800

    oled4

--- build.rs
+++ build.rs
@@ -1,23 +1,3 @@
-#[toml_cfg::toml_config]
-pub struct Config {
-    #[default("localhost")]
-    host: &'static str,
-    #[default("")]
-    product: &'static str,
-    #[default("")]
-    client_id: &'static str,
-    #[default("")]
-    product_id: &'static str,
-    #[default("")]
-    key: &'static str,
-    #[default("")]
-    wifi_ssid: &'static str,
-    #[default("")]
-    wifi_psk: &'static str,
-}
 fn main() {
-    if !std::path::Path::new("cfg.toml").exists() {
-        panic!("You need to create a `cfg.toml` file with your Wi-Fi credentials! Use `cfg.toml.example` as a template.");
-    }
     embuild::espidf::sysenv::output();
 }
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,12 @@ use std::time::{self, SystemTime};
 
 use anyhow::Result;
 use chrono::{DateTime, Utc};
+use embedded_graphics::{
+    mono_font::{ascii::FONT_6X10, MonoTextStyleBuilder},
+    pixelcolor::BinaryColor,
+    prelude::Point,
+    text::{Baseline, Text},
+};
 use embedded_hal::blocking::delay::DelayMs;
 use embedded_svc::mqtt::client::{EventPayload::Error, EventPayload::Received, QoS};
 use esp32c3_wifi::wifi;
@@ -17,66 +23,13 @@ use esp_idf_svc::{
     sntp::{EspSntp, SyncStatus},
     wifi::AuthMethod,
 };
-use hex::encode;
 use log::{error, info, warn};
-use md5::Md5;
-use serde::{Deserialize, Serialize};
-use shtcx::{self, PowerMode};
 
-#[derive(Debug)]
-#[toml_cfg::toml_config]
-pub struct Config {
-    #[default("localhost")]
-    host: &'static str,
-    #[default("")]
-    product: &'static str,
-    #[default("")]
-    client_id: &'static str,
-    #[default("")]
-    product_id: &'static str,
-    #[default("")]
-    key: &'static str,
-    #[default("")]
-    wifi_ssid: &'static str,
-    #[default("")]
-    wifi_psk: &'static str,
-}
-
-use hmac::{Hmac, Mac};
-
-type HmacMd5 = Hmac<Md5>;
-
-pub fn hmac_md5(key: &[u8], data: &[u8]) -> Vec<u8> {
-    let mut mac = <HmacMd5 as Mac>::new_from_slice(key).expect("Invalid key length");
-    mac.update(data);
-    mac.finalize().into_bytes().to_vec()
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-struct MyData {
-    id: String,
-    version: String,
-    sys: Sys,
-    params: Params,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-struct Sys {
-    ack: bool,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-struct Params {
-    CurrentTemperature: Param,
-    CurrentHumidity: Param,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-struct Param {
-    value: f32,
-    time: i64,
-}
-
+use embedded_graphics::Drawable;
+use ssd1306::{
+    mode::DisplayConfig, prelude::DisplayRotation, size::DisplaySize128x64, I2CDisplayInterface,
+    Ssd1306,
+};
 fn main() -> Result<()> {
     esp_idf_svc::sys::link_patches();
     esp_idf_svc::log::EspLogger::initialize_default();
@@ -84,13 +37,10 @@ fn main() -> Result<()> {
     let peripherals = Peripherals::take().unwrap();
     let sysloop = EspSystemEventLoop::take()?;
 
-    let app_config = CONFIG;
-    info!("App Config: {:?}", app_config);
-
     let auth_method = AuthMethod::WPA2Personal;
     let _wifi = match wifi(
-        app_config.wifi_ssid,
-        app_config.wifi_psk,
+        "Xiaomi_CF31",
+        "zjeasy@123",
         auth_method,
         peripherals.modem,
         sysloop,
@@ -109,84 +59,34 @@ fn main() -> Result<()> {
         info!("时间同步完成");
     });
 
-    let sda = peripherals.pins.gpio10;
-    let scl = peripherals.pins.gpio8;
+    let sda = peripherals.pins.gpio5;
+    let scl = peripherals.pins.gpio6;
     let config = I2cConfig::new().baudrate(400.kHz().into());
     let i2c = I2cDriver::new(peripherals.i2c0, sda, scl, &config)?;
-    let mut sht = shtcx::shtc3(i2c);
-    let device_id = sht.device_identifier().unwrap();
-    info!("Device ID SHTC3: {:#02x}", device_id);
-
-    let user_name = format!("{}&{}", app_config.client_id, app_config.product);
-    let password = encode(hmac_md5(app_config.key.as_bytes(), user_name.as_bytes()));
-    let mqtt_config = MqttClientConfiguration {
-        // 配置项，如用户名、密码、客户端ID等
-        client_id: Some(app_config.client_id),
-        username: Some(&user_name),
-        password: Some(&password),
-        protocol_version: Some(MqttProtocolVersion::V3_1_1),
-        network_timeout: time::Duration::from_secs(10),
-        keep_alive_interval: Some(time::Duration::from_secs(60)),
-        ..Default::default()
-    };
-    info!("MQTT 配置: {:#?}", mqtt_config);
-    let broker_url = format!("mqtt://{}", app_config.host);
-    info!("MQTT Broker: {:#?}", broker_url); //192.168.31.76
-    let mut client =
-        EspMqttClient::new_cb(
-            &broker_url,
-            &mqtt_config,
-            |message_event| match message_event.payload() {
-                Received { data, details, .. } => {
-                    info!("Received from MQTT: {:?}", data);
-                    info!("Received from MQTT: {:?}", details);
-                }
-                Error(e) => warn!("Received error from MQTT: {:?}", e),
-                _ => info!("Received from MQTT: {:?}", message_event.payload()),
-            },
-        )?;
-    let post_reply = format!(
-        "/sys/{}/{}/thing/property/post_reply",
-        app_config.client_id, app_config.product_id
-    );
-    info!(" MQTT 属性响应订阅: {}", post_reply);
-    if let Err(err) = client.subscribe(&post_reply, QoS::AtLeastOnce) {
-        error!("MQTT 属性响应订阅失败{err}");
-    };
+    let interface = I2CDisplayInterface::new(i2c);
+    let mut display = Ssd1306::new(interface, DisplaySize128x64, DisplayRotation::Rotate0)
+        .into_buffered_graphics_mode();
+    display.init().unwrap();
+    let text_style = MonoTextStyleBuilder::new()
+        .font(&FONT_6X10)
+        .text_color(BinaryColor::On)
+        .build();
     loop {
-        sht.start_measurement(PowerMode::NormalMode).unwrap();
-        FreeRtos.delay_ms(100u32);
-        let measurement = sht.get_measurement_result().unwrap();
-
-        info!(
-            "TEMP: {:.2} °C | HUM: {:.2} %",
-            measurement.temperature.as_degrees_celsius(),
-            measurement.humidity.as_percent(),
-        );
+        // // 睡眠一秒
+        // FreeRtos::delay_ms(500);
+        // 显示时间
         let st_now = SystemTime::now();
         // Convert to UTC Time
         let dt_now_utc: DateTime<Utc> = st_now.clone().into();
-        let paylod = MyData {
-            id: format!("{}-{}", device_id, dt_now_utc.timestamp_millis()),
-            version: "1.0".to_string(),
-            sys: Sys { ack: true },
-            params: Params {
-                CurrentTemperature: Param {
-                    value: measurement.temperature.as_degrees_celsius(),
-                    time: dt_now_utc.timestamp_millis() as i64,
-                },
-                CurrentHumidity: Param {
-                    value: measurement.humidity.as_percent(),
-                    time: dt_now_utc.timestamp_millis() as i64,
-                },
-            },
-        };
-        let payload = serde_json::to_string(&paylod).unwrap();
-        let topic = format!(
-            "/sys/{}/{}/thing/property/post",
-            app_config.client_id, app_config.product_id
-        );
-        client.publish(&topic, QoS::AtMostOnce, false, payload.as_bytes())?;
-        FreeRtos.delay_ms(500u32);
+        // Format Time String
+        let formatted = format!("{}", dt_now_utc.format("nowtime: %Y-%m-%d %H:%M:%S"));
+       
+        Text::with_baseline(&formatted, Point::new(0, 32), text_style, Baseline::Top)
+            .draw(&mut display)
+            .unwrap();
+        // 刷新屏幕
+        display.flush().unwrap();
+        display.clear_buffer();
     }
+    
 }
